b43b4470f8eb643463e0c1d9cf7daa55c0a143f9
piyush.gyl <piyush2022ug@gmail.com>
2025-02-05 16:02:28 +0530

    fix: teacher's route

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
         <h1 className="mt-5">Manage Students</h1>
         <Link to={"/students"} className="btn btn-primary">View Students</Link>
          <h1 className="mt-5">Manage Teachers</h1>
-        <Link to={"/students"} className="btn btn-primary">View Teachers</Link>
+        <Link to={"/teachers"} className="btn btn-primary">View Teachers</Link>
          <h1 className="mt-5">Manage School</h1>
         <Link to={"/school"} className="btn btn-primary">View School</Link>
       </main>
