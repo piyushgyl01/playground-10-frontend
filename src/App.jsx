f63ae7254466352b578a6db61886dd9d3aadd5cb
piyush.gyl <piyush2022ug@gmail.com>
2025-02-05 15:58:18 +0530

    fix: updated homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-
+import { Link } from "react-router";
 
 export default function App() {
   return (
     <>
-    
-      <h1>Home Page</h1>
+      <main className="container text-center my-4">
+        <h1 className="mt-5">Manage Students</h1>
+        <Link to={"/students"} className="btn btn-primary">View Students</Link>
+         <h1 className="mt-5">Manage Teachers</h1>
+        <Link to={"/students"} className="btn btn-primary">View Teachers</Link>
+         <h1 className="mt-5">Manage School</h1>
+        <Link to={"/school"} className="btn btn-primary">View School</Link>
+      </main>
     </>
   );
 }
